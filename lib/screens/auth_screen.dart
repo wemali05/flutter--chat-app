c37a41d00dd1686745fbef7b6801d5110668dfe7
weali05 <alfredwemali05@gmail.com>
2025-01-06 12:34:03 +0300

    auth buttons and modes to authenticate

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
-const AuthScreen({super.key});
+  const AuthScreen({super.key});
 
-@override
+  @override
   State<StatefulWidget> createState() {
     return _AuthScreenState();
   }
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var _isLogin = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,12 +48,33 @@ class _AuthScreenState extends State<AuthScreen> {
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
                           ),
-
                           TextFormField(
                             decoration: InputDecoration(
                               labelText: 'Password',
                             ),
                             obscureText: true,
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                            ),
+                            child: Text(_isLogin ? 'Login' : 'SignUp'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? 'Create an account'
+                                : 'I already have an account'),
                           ),
                         ],
                       ),
