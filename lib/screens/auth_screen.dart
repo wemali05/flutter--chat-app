d2b49c8442bb090e7068bbf4e569c508a29be47d
weali05 <alfredwemali05@gmail.com>
2025-01-07 13:06:21 +0300

    using imagePicker package

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/user_image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -83,6 +84,7 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
+                          if(!_isLogin) UserImagePicker(),
                           TextFormField(
                             decoration: InputDecoration(
                               labelText: 'Email Address',
--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   const UserImagePicker({super.key});
@@ -10,6 +13,20 @@ class UserImagePicker extends StatefulWidget {
 }
 
 class _UserImagePicker extends State<UserImagePicker> {
+  File? _pickedImageFile;
+
+  void _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(source: ImageSource.camera, maxWidth: 150, imageQuality: 50,);
+
+    if(pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _pickedImageFile = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,10 +34,11 @@ class _UserImagePicker extends State<UserImagePicker> {
         CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
-          foregroundImage: ...,
+          foregroundImage: _pickedImageFile != null ? FileImage(_pickedImageFile!) : null
         ),
         TextButton.icon(
-          onPressed: () {},
+          onPressed: _pickImage,
+          icon: const Icon(Icons.image),
           label: Text(
             'Add Image',
             style: TextStyle(color: Theme.of(context).colorScheme.primary),
