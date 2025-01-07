59d2f44a4d9156ed852d0de6c8c1471d991a62c4
weali05 <alfredwemali05@gmail.com>
2025-01-07 12:38:54 +0300

    user image picker

---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({super.key});
+
+  @override
+  State<UserImagePicker> createState() {
+    return _UserImagePicker();
+  }
+}
+
+class _UserImagePicker extends State<UserImagePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          foregroundImage: ...,
+        ),
+        TextButton.icon(
+          onPressed: () {},
+          label: Text(
+            'Add Image',
+            style: TextStyle(color: Theme.of(context).colorScheme.primary),
+          ),
+        ),
+      ],
+    );
+  }
+}
